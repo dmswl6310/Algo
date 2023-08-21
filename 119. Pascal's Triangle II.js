fe5b79a43330d9d8dd8069dbea30c5ffee9875a2
eunji <ejhwang@seegene.com>
2023-08-21 19:54:13 +0900

    0821

--- 119. Pascal's Triangle II.js
+++ 119. Pascal's Triangle II.js
@@ -1,10 +1,20 @@
-// 미완
 /**
  * @param {number} rowIndex
  * @return {number[]}
  */
 var getRow = function (rowIndex) {
-  return Array;
+  const arr = Array.from(Array(rowIndex + 1), () =>
+    new Array(rowIndex + 1).fill(null)
+  );
+  for (let i = 0; i <= rowIndex; i++) {
+    arr[i][0] = 1;
+    let mid = parseInt(i / 2);
+    for (let j = 1; j < i; j++) {
+      arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+    }
+    arr[i][i] = 1;
+  }
+  return arr[rowIndex];
 };
 
 console.log(getRow(3));
--- 15. 3Sum.js
+++ 15. 3Sum.js
@@ -1,26 +1,52 @@
-// 미완
+//  다사다난
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
 var threeSum = function (nums) {
   nums.sort((a, b) => a - b);
-  const set = new Set();
-  const arr = new Array();
-
+  let arr = [];
+  if (nums[0] > 0 || nums[nums.length - 1] < 0) return [];
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      for (let k = j + 1; k < nums.length; k++) {
-        if (nums[i] + nums[j] + nums[k] === 0)
-          set.add(JSON.stringify([nums[i], nums[j], nums[k]]));
+    let num = nums[i];
+    if (i > 0 && num === nums[i - 1]) continue;
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      let sum = num + nums[left] + nums[right];
+      if (sum > 0) {
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        arr.push([num, nums[left], nums[right]]);
+        while (nums[left] == nums[++left]);
+        while (nums[right] == nums[--right]);
       }
     }
   }
-  set.forEach((element) => {
-    arr.push(JSON.parse(element));
-  });
-
   return arr;
 };
+
+// 시간초과 나오는 로직
+// var threeSum = function (nums) {
+//   nums.sort((a, b) => a - b);
+//   const set = new Set();
+//   const arr = new Array();
+
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       for (let k = j + 1; k < nums.length; k++) {
+//         if (nums[i] + nums[j] + nums[k] === 0)
+//           set.add(JSON.stringify([nums[i], nums[j], nums[k]]));
+//       }
+//     }
+//   }
+//   set.forEach((element) => {
+//     arr.push(JSON.parse(element));
+//   });
+
+//   return arr;
+// };
 
 console.log(threeSum([-1, 0, 1, 2, -1, -4]));
