1c1671041b0008ad8f6e5001f9f20fb5ce0d502a
eunji <dmswl6310@gmail.com>
2023-09-07 06:38:11 +0900

    09/07

--- 11. Container With Most Water.js
+++ 11. Container With Most Water.js
@@ -2,5 +2,24 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function (height) {};
+var maxArea = function (height) {
+    let left=0;
+    let right=height.length-1;
+
+    let maxWater=0;
+while(left<right){
+    const heightLeft=height[left];
+    const heightRight=height[right];
+    const width=right-left;
+    const volume=(Math.min(heightLeft,heightRight))*width;
+    if(volume>maxWater) maxWater=volume;
+
+    if(heightLeft>=heightRight){
+        while(height[--right]<=heightRight);
+    }else{
+        while(height[++left]<=heightLeft);
+    }
+}
+return maxWater;
+};
 console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
