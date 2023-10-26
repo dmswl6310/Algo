3489987a0c678b2cb1a3c9ce29518098d9c1647c
황은지 <dmswl6310@naver.com>
2023-10-26 19:13:25 +0900

    1026

--- 383. Ransom Note.java
+++ 383. Ransom Note.java
@@ -1,6 +1,28 @@
+import java.util.Map;
+
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        
+        Map<Character,Integer> map=new HashMap();
+
+        for(int i=0;i<magazine.length();i++){
+            char alphabet=magazine.charAt(i);
+            if(map.containsKey(alphabet)){
+                map.replace(alphabet,map.get(alphabet)+1);
+            }else{
+                map.put(alphabet,1);
+            }
+        }
+
+        for(int i=0;i<ransomNote.length();i++){
+            char ransom=ransomNote.charAt(i);
+
+            if(map.containsKey(ransom)){
+                if(map.get(ransom)<=0) return false;
+                 map.replace(ransom,map.get(ransom)-1);
+            }else{
+                return false;
+            }
+        }
+        return true;
     }
 }
-
